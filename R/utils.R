5b267fda6e74162a01679a428808acc3bc09ae56
Jonathon Love <jon@thon.cc>
2017-01-07 13:16:41 +1100

    Improvement to escape_vars()

--- R/utils.R
+++ R/utils.R
@@ -2,6 +2,14 @@
 escape_vars <- function(names) {
   if (length(names) == 0)
     return(names)
-  paste0('`', gsub('\`', '\\\\`', names), '`')
+  names <- sapply(names, function(name) {
+    if (make.names(name) != name) {
+      name <- gsub('\\', '\\\\', name, fixed=TRUE)
+      name <- gsub('`',  '\\`',  name, fixed=TRUE)
+      name <- paste0('`', name, '`')
+    }
+    name
+  }, USE.NAMES=FALSE)
+  names
 }
 

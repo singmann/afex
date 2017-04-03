359aecf7c895c39ac9bb539b9cd0b7b5731cd207
singmann <singmann@gmail.com>
2017-04-03 20:03:10 +0200

    added tidyr to suggests because of vignette

--- R/afex-package.R
+++ R/afex-package.R
@@ -4,7 +4,7 @@
 #' Package: \tab afex\cr
 #' Type: \tab Package\cr
 #' Version: \tab 0.17-5\cr
-#' Date: \tab 2017-04-01\cr
+#' Date: \tab 2017-04-03\cr
 #' Depends: \tab R (>= 3.1.0), lme4 (>= 1.1-8), lsmeans (>= 2.17)\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>=2)\cr

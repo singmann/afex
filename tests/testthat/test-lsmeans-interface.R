8e8f77ec396a834dd4a0f3707b8dd8674bfbebf7
singmann <henrik.singmann@psychologie.uni-freiburg.de>
2017-07-28 15:43:32 +0200

    lsmeans now works with Type 2 for all methods.

--- tests/testthat/test-lsmeans-interface.R
+++ tests/testthat/test-lsmeans-interface.R
@@ -23,3 +23,22 @@ test_that("mixed works with lsmeans", {
   m3 <- mixed(response ~ instruction +(inference|id), sk2011.1, progress = FALSE)
   expect_is(lsmeans(m3, ~ instruction), "lsmobj")
 })
+
+test_that("mixed works with type=2 and all methods", {
+  data(md_16.4)
+  mixed_kr <- mixed(induct ~ cond + (1|room:cond), md_16.4, type=2, method="KR", progress = FALSE)
+  expect_is(lsmeans(mixed_kr, specs = c("cond")), "lsmobj")
+  
+  mixed_s <- mixed(induct ~ cond + (1|room:cond), md_16.4, type=2, method="S", progress = FALSE)
+  expect_is(lsmeans(mixed_s, specs = c("cond")), "lsmobj")
+  
+  mixed_lrt <- mixed(induct ~ cond + (1|room:cond), md_16.4, type=2, method="LRT", progress = FALSE)
+  expect_is(lsmeans(mixed_lrt, specs = c("cond")), "lsmobj")
+  
+  mixed_pb <- mixed(induct ~ cond + (1|room:cond), md_16.4, type=2, method="PB", progress = FALSE, 
+                    args_test = list(nsim = 10))
+  expect_is(lsmeans(mixed_pb, specs = c("cond")), "lsmobj")
+  
+  mixed_oldkr <- mixed(induct ~ cond + (1|room:cond), md_16.4, type=2, method="nested-KR", progress = FALSE)
+  expect_is(lsmeans(mixed_oldkr, specs = c("cond")), "lsmobj")
+})

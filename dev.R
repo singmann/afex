1d4153b38217c270b880fc04c6434c39755fb1d0
singmann <singmann@gmail.com>
2018-01-08 10:26:37 +0100

    version bump

--- dev.R
+++ dev.R
@@ -38,7 +38,7 @@ R.libs <- "./packages/library"
 closeAllConnections()
 roxy.package(
 	pck.source.dir = pkg.src.dir,
-	pck.version = "0.19-0",
+	pck.version = "0.19-1",
 	pck.description = data.frame(
 		Package = "afex",
 		Type = "Package",

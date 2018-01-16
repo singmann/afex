282fc639efe8f14a67b40496f123e53c186e66e0
Henrik Singmann <singmann@gmail.com>
2018-01-16 15:18:58 +0100

    updated mixed examples, added MEMMS and effects to suggests

--- dev.R
+++ dev.R
@@ -54,7 +54,7 @@ roxy.package(
         person(given=\"Jonathan\", family=\"Love\", role=c(\"ctb\"))
     )",
 		Depends = "R (>= 3.1.0), lme4 (>= 1.1-8), emmeans",
-		Suggests = "xtable, parallel, plyr, optimx, nloptr, knitr, rmarkdown, lattice, latticeExtra, multcomp, testthat, mlmRev, dplyr, tidyr, dfoptim, Matrix, psych, ggplot2",
+		Suggests = "xtable, parallel, plyr, optimx, nloptr, knitr, rmarkdown, lattice, latticeExtra, multcomp, testthat, mlmRev, dplyr, tidyr, dfoptim, Matrix, psych, ggplot2, MEMSS, effects",
 		Imports = "stringr, coin, pbkrtest (>= 0.4-1), lmerTest, car, reshape2, stats, methods",
 		Description = "Convenience functions for analyzing factorial experiments using ANOVA or mixed models. aov_ez(), aov_car(), and aov_4() allow specification of between, within (i.e., repeated-measures), or mixed between-within (i.e., split-plot) ANOVAs for data in long format (i.e., one observation per row), aggregating multiple observations per individual and cell of the design. mixed() fits mixed models using lme4::lmer() and computes p-values for all fixed effects using either Kenward-Roger or Satterthwaite approximation for degrees of freedom (LMM only), parametric bootstrap (LMMs and GLMMs), or likelihood ratio tests (LMMs and GLMMs). afex uses type 3 sums of squares as default (imitating commercial statistical software).",
 		URL = "http://afex.singmann.science/, https://github.com/singmann/afex",

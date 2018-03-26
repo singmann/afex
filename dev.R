bb72a06750a6306f88866e07ed7375f9b24fd096
Henrik Singmann <singmann@gmail.com>
2018-03-26 19:10:32 +0200

    version bump and more provision for new reverse dependency checks

--- dev.R
+++ dev.R
@@ -38,7 +38,7 @@ R.libs <- "./packages/library"
 closeAllConnections()
 roxy.package(
 	pck.source.dir = pkg.src.dir,
-	pck.version = "0.20-1",
+	pck.version = "0.20-2",
 	pck.description = data.frame(
 		Package = "afex",
 		Type = "Package",

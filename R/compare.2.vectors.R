b1692c4e153770ac629c168f3521433efcbcf181
Henrik Singmann <singmann@gmail.com>
2018-04-15 16:35:43 +0200

    preparation for removing coin from IMPORTS

--- R/compare.2.vectors.R
+++ R/compare.2.vectors.R
@@ -15,7 +15,7 @@
 #' @param tests Which tests to report, parametric or nonparamteric? The default \code{c("parametric", "nonparametric")} reports both. See details. (Arguments may be abbreviated).
 #' @param alternative a character, the alternative hypothesis must be one of \code{"two.sided"} (default), \code{"greater"} or \code{"less"}. You can specify just the initial letter, will be passed to all functions.
 #' @param coin logical or character. Should (permutation) tests from the \pkg{coin} package be reported? Default is \code{TRUE} corresponding to all implemented tests. \code{FALSE} calculates no tests from \pkg{coin}. A character vector may include any of the following (potentially abbreviated) implemented tests (see also Details): \code{c("permutation", "Wilcoxon", "median")}
-#' @param perm.distribution \code{distribution} argument to \pkg{coin}, see \code{\link[coin]{NullDistribution}} or , \code{\link[coin]{IndependenceTest}}. Defaults to \code{approximate(100000)} indicating an approximation of the excat conditional distribution with 100.000 Monte Carlo samples. One can use \code{"exact"} for small samples and if \code{paired = FALSE}.
+#' @param perm.distribution \code{distribution} argument to \pkg{coin}, see \code{\link[coin]{NullDistribution}} or , \code{\link[coin]{IndependenceTest}}. If missing, defaults to \code{coin::approximate(100000)} indicating an approximation of the excat conditional distribution with 100.000 Monte Carlo samples. One can use \code{"exact"} for small samples and if \code{paired = FALSE}.
 #' @param wilcox.exact \code{exact} argument to \code{\link{wilcox.test}}.
 #' @param wilcox.correct \code{correct} argument to \code{\link{wilcox.test}}.
 #'
@@ -33,7 +33,7 @@
 #' @return a list with up to two elements (i.e., \code{paramteric} and/or \code{nonparamteric}) each containing  a \code{data.frame} with the following columns: \code{test}, \code{test.statistic}, \code{test.value}, \code{test.df}, \code{p}.
 #'
 #' @export compare.2.vectors
-#' @importFrom coin oneway_test wilcox_test median_test approximate statistic pvalue
+# @importFrom coin oneway_test wilcox_test median_test approximate statistic pvalue
 #' @importFrom stats t.test wilcox.test
 #' @example examples/examples.compare.R
 #' 
@@ -43,7 +43,7 @@
 compare.2.vectors <- function(x, y, paired = FALSE, na.rm = FALSE, 
                               tests = c("parametric", "nonparametric"), 
                               coin = TRUE, alternative = "two.sided", 
-                              perm.distribution = approximate(100000), 
+                              perm.distribution, 
                               wilcox.exact = NULL, wilcox.correct = TRUE) {
 	tests <- match.arg(tests, c("parametric", "nonparametric"), several.ok = TRUE)
 	if (na.rm) {
@@ -85,9 +85,16 @@ compare.2.vectors <- function(x, y, paired = FALSE, na.rm = FALSE,
 		                            test.value = res.wilcox[["statistic"]], 
 		                            test.df = NA, p = res.wilcox[["p.value"]], 
 		                            stringsAsFactors = FALSE)
+		if (!(coin == FALSE) && !requireNamespace(coin)) {
+		  warning("package coin necessary if coin != FALSE.")
+		  coin <- FALSE
+		}
 		if (!(coin == FALSE)) {
 			dv <- c(x, y)
 			iv <- factor(rep(c("A", "B"), c(length(x), length(y))))
+			if (missing(perm.distribution)) {
+			  perm.distribution <- coin::approximate(100000)
+			}
 			if (paired) {
 				id <- factor(rep(1:length(x), 2))
 				formula.coin <- as.formula(dv ~ iv | id)
@@ -95,38 +102,43 @@ compare.2.vectors <- function(x, y, paired = FALSE, na.rm = FALSE,
 			if (isTRUE(coin)) coin <- implemented.tests
 			else coin <- match.arg(coin, implemented.tests, several.ok = TRUE)
 			tryCatch(if ("permutation" %in% coin) {
-				res.perm <- oneway_test(formula.coin, distribution=perm.distribution, 
-				                        alternative = alternative)
+			  res.perm <- coin::oneway_test(formula.coin, 
+			                                distribution=perm.distribution, 
+			                                alternative = alternative)
 				nonparametric <- rbind(nonparametric, 
 				                       data.frame(test = "permutation", 
 				                                  test.statistic = "Z", 
-				                                  test.value = statistic(res.perm), 
-				                                  test.df = NA, p = pvalue(res.perm)[1],
+				                                  test.value = 
+				                                    coin::statistic(res.perm), 
+				                                  test.df = NA, 
+				                                  p = coin::pvalue(res.perm)[1],
 				                                  stringsAsFactors = FALSE))
 			}, error = function(e) 
 			  warning(paste("coin::permutation test failed:", e)))
 			tryCatch(if ("Wilcoxon" %in% coin) {
-				res.coin.wilcox <- wilcox_test(formula.coin, 
-				                               distribution=perm.distribution, 
-				                               alternative = alternative)
+			  res.coin.wilcox <- coin::wilcox_test(formula.coin, 
+			                                       distribution=perm.distribution, 
+			                                       alternative = alternative)
 				nonparametric <- rbind(nonparametric, 
 				                       data.frame(test = "coin::Wilcoxon", 
 				                                  test.statistic = "Z", 
 				                                  test.value = 
-				                                    statistic(res.coin.wilcox), 
+				                                    coin::statistic(res.coin.wilcox), 
 				                                  test.df = NA, 
-				                                  p = pvalue(res.coin.wilcox)[1], 
+				                                  p = coin::pvalue(res.coin.wilcox)[1], 
 				                                  stringsAsFactors = FALSE))
 			}, error = function(e) warning(paste("coin::Wilcoxon test failed:", e)))
 			tryCatch(if ("median" %in% coin) {
-				res.median <- median_test(formula.coin, distribution=perm.distribution, 
-				                          alternative = alternative)
+				res.median <- coin::median_test(formula.coin, 
+				                                distribution=perm.distribution, 
+				                                alternative = alternative)
 				nonparametric <- rbind(nonparametric, 
 				                       data.frame(test = "median", 
 				                                  test.statistic = "Z", 
-				                                  test.value = statistic(res.median), 
+				                                  test.value = 
+				                                    coin::statistic(res.median), 
 				                                  test.df = NA, 
-				                                  p = pvalue(res.median)[1], 
+				                                  p = coin::pvalue(res.median)[1], 
 				                                  stringsAsFactors = FALSE))
 			}, error = function(e) warning(paste("coin::median test failed:", e)))
 		}

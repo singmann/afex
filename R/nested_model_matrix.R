1526eeebb7befb7207edc2d080cfbfbcba926f33
Henrik Singmann <singmann@gmail.com>
2018-08-04 19:01:56 +0200

    first version of nested_model_matrix

--- R/nested_model_matrix.R
+++ R/nested_model_matrix.R
@@ -2,62 +2,64 @@
 nested_model_matrix <- function(fixed_formula, data, 
                                 random_formula,
                                 type = 3,
-                                per_parameter) {
+                                test_intercept = FALSE,
+                                per_parameter,
+                                na.action) {
   
   ## to avoid problems with tibbles:
   data <- as.data.frame(data)
   
-  ## get informations
-  #browser()
   
-  mc <- match.call()
-  formula.f <- as.formula(fixed_formula)
+  #mc <- match.call()
+  formula <- as.formula(fixed_formula)
   if (!inherits(fixed_formula, "formula")) 
     message("Formula (the first argument) converted to formula.")
-  dv <- as.character(formula.f)[[2]]
-  all.terms <- attr(terms(formula.f), "term.labels")
-  effect.order <- attr(terms(formula.f), "order")
-  max.effect.order <- max(effect.order)
-  random <- str_c(str_c("(", all.terms[grepl("\\|", all.terms)], ")"), 
-                  collapse = " + ")
-  rh2 <- nobars(formula.f)
-  rh2[[2]] <- NULL
-  m.matrix <- model.matrix(rh2, data = data)
-  fixed.effects <- attr(terms(rh2, data = data), "term.labels")
-  mapping <- attr(m.matrix, "assign")
-  fixed.vars <- all.vars(rh2)
-  # check for missing values in variables used:
-  if (nrow(m.matrix) != nrow(data)) {
-    data <- model.frame(
-      as.formula(str_c(vars.to.check[1], 
-                       "~", 
-                       str_c(vars.to.check[-1], collapse = "+"))), 
-      data = data)
-    m.matrix <- model.matrix(rh2, data = data)
-    warning(str_c("Due to missing values, reduced number of observations to ", 
-                  nrow(data)))
-    if(set_data_arg) {
-      warning("Due to missing values, set_data_arg set to FALSE.")
-      set_data_arg <- FALSE
-    }
+  dv <- as.character(formula)[[2]]
+
+  full_formula <- reformulate(unique(c(all.vars(formula[[3]]), 
+                                       all.vars(random_formula))), 
+              response = formula[[2]])
+  new_data <- model.frame(full_formula, data = data, na.action = na.action)
+  
+  all_terms <- attr(terms(formula), "term.labels")
+  effect_order <- attr(terms(formula), "order")
+  max_effect_order <- max(effect_order)
+  
+  m_matrix <- model.matrix(formula, data = new_data)
+
+  fixed_effects <- attr(terms(formula, data = data), "term.labels")
+  mapping <- attr(m_matrix, "assign")
+  fixed_vars <- all.vars(formula)[-1]
+  
+  out_data <- data.frame(new_data, m_matrix)
+  tmp_colnames <- colnames(out_data)[-seq_len(ncol(new_data))]
+  
+  if (attr(terms(formula, data = data), "intercept") == 1) {
+    fixed_effects <- c("(Intercept)", fixed_effects)
   }
   
-  if (attr(terms(rh2, data = data), "intercept") == 1) 
-    fixed.effects <- c("(Intercept)", fixed.effects)
-  
-  formulas <- vector("list", length(fixed.effects) + 1)
-  formulas[[1]] <- mf[["formula"]]
-  for (i in seq_along(fixed.effects)) {
-    tmp.columns <- str_c(deparse(-which(mapping == (i-1))), collapse = "")
-    formulas[[i+1]] <- 
-      as.formula(str_c(dv, "~ 0 + m.matrix[,", tmp.columns, "] +", random))
+  formulas <- vector("list", length(fixed_effects) + 1)
+  formulas[[1]] <- formula
+  for (i in seq_along(fixed_effects)) {
+    formulas[[i+1]] <- reformulate(tmp_colnames[!(mapping == (i-1))], 
+                response = dv, intercept = FALSE)
   }
-  names(formulas) <- c("full_model", fixed.effects)
-  if (!test_intercept && fixed.effects[1] == "(Intercept)") {
-    fixed.effects <- fixed.effects[-1]
+  names(formulas) <- c("full_model", fixed_effects)
+  if (!test_intercept && fixed_effects[1] == "(Intercept)") {
+    fixed_effects <- fixed_effects[-1]
     formulas[["(Intercept)"]] <- NULL
   }
-
+  
+  full_formulas <- lapply(formulas, function(x)
+    as.formula(paste(deparse(x), deparse(random_formula[[2]]))))
+  
+  out <- list(
+    formulas = full_formulas,
+    data = out_data,
+    fixed_formulas = formulas
+  )
+  
+  return(out)
   
 }
 

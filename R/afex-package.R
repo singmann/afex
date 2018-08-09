84efe8ac9cfe2ac1cb0d40ea3eb7d188add8e0c9
Henrik Singmann <singmann@gmail.com>
2018-08-09 11:15:39 +0200

    cleanup of afex-package documentation

--- R/afex-package.R
+++ R/afex-package.R
@@ -1,21 +1,10 @@
 #' \packageDescription{afex}
 #'
 #' The DESCRIPTION file:
-#' \tabular{ll}{
-#' Package: \tab afex\cr
-#' Type: \tab Package\cr
-#' Version: \tab 0.22-0\cr
-#' Date: \tab 2018-08-09\cr
-#' Depends: \tab R (>= 3.1.0), lme4 (>= 1.1-8), emmeans (>= 1.1.2)\cr
-#' Encoding: \tab UTF-8\cr
-#' License: \tab GPL (>=2)\cr
-#' URL: \tab http://afex.singmann.science/, https://github.com/singmann/afex\cr
-#' }
+#' \packageDESCRIPTION{afex}
 #'
 #' @title
 #' \packageTitle{afex}
-#' @author
-#' \packageAuthor{afex}
 #'
 #' Maintainer: \packageMaintainer{afex}
 "_PACKAGE"

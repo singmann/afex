38d176878e245d6f96cac18c9be754dd5a70e9da
Henrik Singmann <singmann@gmail.com>
2018-08-09 10:36:51 +0200

    rebuild documentation, version bump

--- R/afex-package.R
+++ R/afex-package.R
@@ -1,29 +1,21 @@
-#' Analysis of Factorial Experiments.
+#' \packageDescription{afex}
 #'
+#' The DESCRIPTION file:
 #' \tabular{ll}{
 #' Package: \tab afex\cr
 #' Type: \tab Package\cr
-#' Version: \tab 0.21-2\cr
-#' Date: \tab 2018-06-24\cr
+#' Version: \tab 0.22-0\cr
+#' Date: \tab 2018-08-09\cr
 #' Depends: \tab R (>= 3.1.0), lme4 (>= 1.1-8), emmeans (>= 1.1.2)\cr
 #' Encoding: \tab UTF-8\cr
 #' License: \tab GPL (>=2)\cr
 #' URL: \tab http://afex.singmann.science/, https://github.com/singmann/afex\cr
 #' }
 #'
-#' Convenience functions for analyzing factorial experiments using ANOVA or mixed models.
-#' aov_ez(), aov_car(), and aov_4() allow specification of between, within (i.e.,
-#' repeated-measures), or mixed between-within (i.e., split-plot) ANOVAs for data in long format (i.e.,
-#' one observation per row), aggregating multiple observations per individual and cell of the
-#' design. mixed() fits mixed models using lme4::lmer() and computes p-values for all fixed
-#' effects using either Kenward-Roger or Satterthwaite approximation for degrees of freedom
-#' (LMM only), parametric bootstrap (LMMs and GLMMs), or likelihood ratio tests (LMMs and
-#' GLMMs). afex uses type 3 sums of squares as default (imitating commercial statistical software).
+#' @title
+#' \packageTitle{afex}
+#' @author
+#' \packageAuthor{afex}
 #'
-#' @aliases afex-package
-#' @name afex-package
-#' @docType package
-#' @title The afex Package
-#' @author Henrik Singmann, Ben Bolker, Jake Westfall, Frederik Aust, with contributions from Søren Højsgaard, John Fox, Michael A. Lawrence, Ulf Mertens, Jonathon Love, Russell Lenth, Rune Haubo Bojesen Christensen
-#' @keywords package
-NULL
+#' Maintainer: \packageMaintainer{afex}
+"_PACKAGE"

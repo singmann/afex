e23d62de76a619f99ce38d3b967e4029b692b27d
Henrik Singmann <singmann@gmail.com>
2018-09-17 20:04:35 +0200

    first version of afex_plot with documentation

--- tests/testthat/test-afex_plot-basics.R
+++ tests/testthat/test-afex_plot-basics.R
@@ -5,15 +5,15 @@ test_that("all input type works", {
   # estimate mixed ANOVA on the full design:
   a1 <- aov_car(value ~ treatment * gender + Error(id/(phase*hour)), 
                 data = obk.long, observed = "gender")
-  em1 <- get_emmeans(a1, ~phase*hour, ~treatment+gender)
-  em2 <- get_emmeans(a1, c("phase", "hour"), ~treatment+gender)
-  em3 <- get_emmeans(a1, ~phase*hour, c("treatment", "gender"))
-  em4 <- get_emmeans(a1, c("phase", "hour"), c("treatment", "gender"))
+  em1 <- afex_plot(a1, ~phase*hour, ~treatment+gender, return = "data")
+  em2 <- afex_plot(a1, c("phase", "hour"), ~treatment+gender, return = "data")
+  em3 <- afex_plot(a1, ~phase*hour, c("treatment", "gender"), return = "data")
+  em4 <- afex_plot(a1, c("phase", "hour"), c("treatment", "gender"), return = "data")
   expect_equal(em1, em2)
   expect_equal(em1, em3)
   expect_equal(em1, em4)
   
-  em5 <- get_emmeans(a1, c("phase", "hour"))
-  em6 <- get_emmeans(a1, ~phase*hour)
-  expect_equal(em5, em6)
+  # em5 <- afex_plot(a1, c("phase", "hour"), return = "data")
+  # em6 <- afex_plot(a1, ~phase*hour, return = "data")
+  # expect_equal(em5, em6)
 })

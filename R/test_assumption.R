39facb66a5ac17259bf885e7b390c6f269052446
Henrik Singmann <singmann@gmail.com>
2019-05-04 15:33:27 +0100

    corrected error in Mauchly test, combined assumption tests, added examples

--- R/test_assumption.R
+++ R/test_assumption.R
@@ -1,22 +1,26 @@
-#' Levene's Test for Homogeneity
-#'
-#' Computes Levene's test for homogeneity of variance across groups via \code{car::leveneTest}.
+#' Assumption Tests for ANOVAs
+#' 
+#' \code{test_levene} computes Levene's test for homogeneity of variances across
+#' groups via \code{car::leveneTest}. \code{test_sphericity} computes Mauchly
+#' test of sphericity via \code{car::Anova}.
 #'
 #' @param afex_aov \code{afex_aov} object.
-#' @param center Function to compute the center of each group; \code{mean} (the default) gives the original Levene's test.
-#' @param ... passed to \code{car::leveneTest}
+#' @param center Function to compute the center of each group; \code{mean} (the
+#'   default) gives the original Levene's test.
+#' @param ... passed to \code{\link[car]{leveneTest}}
 #'
 #' @author Mattan S. Ben-Shachar
 #' 
-#' @examples 
-#' fit_value <- aov_car(value ~ treatment*gender + Error(id), data = obk.long)
+#' @rdname test_assumptions 
 #' 
-#' test_levene(fit_value)
-#'
+#' @example examples/examples.test_assumptions.R
+
+
 #' @export
+#' @rdname assumption_tests 
 test_levene <- function(afex_aov, center = mean, ...){
   if (length(attr(afex_aov,'between'))==0) {
-    stop("Levene test is only aplicable to models with between subject factors.")
+    stop("Levene test is only aplicable to ANOVAs with between-subjects factors.")
   }
   data <- afex_aov$data$long
   dv <- attr(afex_aov,'dv')
@@ -31,23 +35,11 @@ test_levene <- function(afex_aov, center = mean, ...){
   car::leveneTest(form,ag_data, center = center, ...)
 }
 
-#' Mauchly Test of Sphericity
-#'
-#' Computes Mauchly test of sphericity via \code{car::Anova}.
-#'
-#' @param afex_aov \code{afex_aov} object.
-#'
-#' @author Mattan S. Ben-Shachar
-#' 
-#' @examples 
-#' fit_value <- aov_car(value ~ treatment * gender + Error(id/(phase*hour)), data = obk.long)
-#' 
-#' test_sphericity(fit_value)
-#'
 #' @export
+#' @rdname assumption_tests 
 test_sphericity<- function(afex_aov){
-  if (length(attr(between_1,'within'))==0) {
-    stop("Mauchly Test of Sphericity is only aplicable to models with within subject factors.")
+  if (length(attr(afex_aov,'within'))==0) {
+    stop("Mauchly Test of Sphericity is only aplicable to ANOVAs with within-subjects factors.")
   }
   summary(afex_aov)$sphericity.tests
 }

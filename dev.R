937ce4cb4583096221e16540c9d37cfa22e2d68d
Henrik Singmann <singmann@gmail.com>
2020-08-24 08:25:18 +0200

    rebuild latest package version

--- dev.R
+++ dev.R
@@ -4,7 +4,8 @@ load_all()
 
 options(error = NULL)
 devtools::test()
-devtools::build(args = "--compact-vignettes=both") # R CMD build afex --compact-vignettes="gs+qpdf"
+devtools::build(args = "--compact-vignettes=both", 
+                path = "development/") # R CMD build afex --compact-vignettes="gs+qpdf"
 document()
 check()
 
